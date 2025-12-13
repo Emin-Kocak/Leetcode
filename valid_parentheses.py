545fc28bfcbc88bbcc490742cd2fc0c3aeab1cf7
Emin <muhamnet1605@gmail.com>
2025-12-13 23:53:22 +0300

    solve the valid parentheses problem

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -17,5 +17,21 @@ class Solution(object):
                     return False
         return len(stack) == 0
 
+''' Alternative Implementation
+class Solution(object):
+    def isValid(self, s):
+        control = {'}':'{', ']':'[', ')':'('}
+        stack = []
+
+        for char in s:
+            if char in control.values():
+                stack.append(char)
+            else:
+                if not stack or stack[-1] != control[char]:
+                    return False
+                stack.pop()
+        return not stack
+'''
+
 Solution = Solution()
 print(Solution.isValid("([])")) 
